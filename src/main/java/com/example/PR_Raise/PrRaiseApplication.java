f5ec00ae25677d7d053addec1f3b51c15d142201
sohel730 <momin8805887375@gmail.com>
2026-05-03 16:13:05 +0530

    iniatial upload on main branch

---
+++ src/main/java/com/example/PR_Raise/PrRaiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.PR_Raise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrRaiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrRaiseApplication.class, args);
+	}
+
+}
